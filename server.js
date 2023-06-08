2c843debfefbdf7da3cc8a16146b6bd6c633baab
kdb11 <112870790+kdb11@users.noreply.github.com>
2023-06-08 14:17:00 +0200

    pos

--- server.js
+++ server.js
@@ -11,3 +11,19 @@ const blockchain = new Blockchain();
 app.get('/blocks', (req, res) => {
   res.json(blockchain.chain);
 });
+
+app.post('/blocks', (req, res) => {
+  const data = req.body.data;
+  const newBlock = new Block(
+    blockchain.chain.length,
+    new Date().toISOString(),
+    data
+  );
+
+  if (blockchain.isChainValid()) {
+    blockchain.addBlock(newBlock);
+    res.json(newBlock);
+  } else {
+    res.status(500).json({ error: 'Blockchain is invalid.' });
+  }
+});
