7b97dc6da805047c99a43d427f3dcfc859b77a4a
kdb11 <112870790+kdb11@users.noreply.github.com>
2023-06-08 14:09:06 +0200

    valid

--- block.js
+++ block.js
@@ -18,6 +18,23 @@ class Block {
     ).toString();
   }
 
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
 }
 
 module.exports = Block;
